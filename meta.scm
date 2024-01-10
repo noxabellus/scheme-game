947978d4b949b7ada7a4f0d16bd6f8c4f016c7dd
noxabellus <noxabellus@gmail.com>
2024-01-10 17:04:19 -0600

    meta script for build/run/clean

---
+++ meta.scm
@@ -0,0 +1,26 @@
+#! /usr/bin/csi -script
+
+(import (scheme base)
+        (scheme process-context)
+        (prefix (shell) shell:))
+
+(define args (cdr (command-line)))
+(define (run) (shell:run (cd build && ./scheme-game)))
+(define (build)
+    (clean)
+    (shell:run (mkdir build && cp scheme-game.egg ./build && cd build && chicken-install -n)))
+(define (clean) (shell:run (rm -rf build)))
+(define (help exit-code)
+    (display "Usage: ./meta.scm [help | run | build | build-and-run | clean]\n")
+    (exit exit-code))
+
+(unless (= 1 (length args))
+    (help 1))
+    
+(case (string->symbol (car args))
+    ((help) (help 0))
+    ((run) (run))
+    ((build) (build))
+    ((build-and-run) (begin (build) (run)))
+    ((clean) (clean))
+    (else (help 1)))
